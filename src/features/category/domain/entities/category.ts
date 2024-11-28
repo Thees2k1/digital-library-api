512d6d55af964ed3c0cd2f9abfea77a542d4dfab
Thees2k1 <tongnguyenthe@gmail.com>
2024-11-28 13:23:20 +0700

    feat: implement AuthorEntity, BookEntity, CategoryEntity, and ReviewEntity with necessary methods

--- src/features/category/domain/entities/category.ts
+++ src/features/category/domain/entities/category.ts
@@ -1,2 +1,19 @@
+class CategoryEntity {
+  private id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
 
+  constructor(data: CategoryEntity) {
+    this.id = data.id;
+    this.name = data.name;
+    this.description = data.description;
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
+  }
 
+  static create(data: CategoryEntity): CategoryEntity {
+    return new CategoryEntity(data);
+  }
+}
